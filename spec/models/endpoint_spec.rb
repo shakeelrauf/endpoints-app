09dbf739f5cc8d9b5994e8ca04bb873597fdebdd
Shakeel Rauf <shakeel.rauf@healthwire.pk>
2022-10-10 22:05:28 +0500

    Add spec for endpoint model.

--- spec/models/endpoint_spec.rb
+++ spec/models/endpoint_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Endpoint, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(verb: "POST",
+                        path: "/endpoints"
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a verb" do
+    subject.verb = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a path" do
+    subject.path = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid withour a valid verb"
+  it "is not valid withour a valid path"
 end
